ddad9d04eed33edbe62e67f9510d5711fa64f90f
bmb0205 <bburciag@uncc.edu>
2016-05-24 13:43:25 -0400

    ny times links almost done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function loadData() {
     // obtain variables from jQuery
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
+    var $nyContainer = $('.nytimes-container');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
@@ -17,12 +18,15 @@ function loadData() {
 
     // Once address is submitted, fonts change to white, a new greeting message appears and
     // and the Google street view of the address appears as the background
-    var message = "Want to check out " + $street + ", " + $city + "?";
-    $('#greeting').html(message).css("color", "#ffffff");
-    $('.nytimes-container').css("color", "#ffffff");
+    var message = "Want to check out " + $street + ",<br> " + $city + "?";
+    $greeting.html(message).css("color", "#ffffff");
+    $nyContainer.css("color", "#ffffff");
     $('.wikipedia-container').css("color", "#ffffff");
-    $body.append('<img class="bgimage" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +
-        $address + '">');
+
+    var apiURL = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + $address + "\"";
+    $('.bgimage').attr("src", apiURL);
+    // $body.append('<img class="bgimage" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +
+    //     $address + '">');
 
 
     // Set NY Times Article Search URL using obtained API key and submitted city
@@ -30,10 +34,19 @@ function loadData() {
     var URL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + $city + '&api-key=' + apikey;
     $.ajax({
         url: URL,
-        method: 'GET'
-    }).success(function(result) {
-        console.log(result);
-    }).fail(function(err) {
+        method: 'GET',
+        dataType: 'json'
+    }).success(function(data) {
+        $nyContainer.css("height", "60px");
+        $nytElem.css("background-color", "rgba(51, 51, 51, 0.9)");
+        $nytElem.css("height", "600px");
+        $nytElem.css("overflow", "auto");
+        $.each(data.response.docs, function(i, item) {
+            console.log(item.web_url);
+            console.log(item.headline.main);
+            $nytElem.append('<p>' + item.headline.main + '<br>' + item.web_url + '</p>');
+        });
+    }).error(function(err) {
         throw err;
     });
     return false;
